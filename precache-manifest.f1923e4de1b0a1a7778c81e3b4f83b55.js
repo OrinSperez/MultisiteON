dc50b9af88e370e084b07e553e008649cd46f20e
Orlin <orlinsperez28@gmail.com>
2020-09-13 13:08:39 -0400

    Updates

---
+++ precache-manifest.f1923e4de1b0a1a7778c81e3b4f83b55.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cab86b157d5f7668eccff2edcd5e698",
+    "url": "/MultisiteON/index.html"
+  },
+  {
+    "revision": "92bd16967b58fd9bcec6",
+    "url": "/MultisiteON/static/css/main.efcb08e9.chunk.css"
+  },
+  {
+    "revision": "8f1e8686466d1d395fbc",
+    "url": "/MultisiteON/static/js/2.c3cad723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MultisiteON/static/js/2.c3cad723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92bd16967b58fd9bcec6",
+    "url": "/MultisiteON/static/js/main.b8f478ab.chunk.js"
+  },
+  {
+    "revision": "d2c531cba490afc2cee1",
+    "url": "/MultisiteON/static/js/runtime-main.8b41c8b3.js"
+  },
+  {
+    "revision": "5ac6fe3634edc0e0ca34e876efc3c28f",
+    "url": "/MultisiteON/static/media/Pc.5ac6fe36.png"
+  },
+  {
+    "revision": "2427c94dc94d9bc77f69d3171f22dd84",
+    "url": "/MultisiteON/static/media/Phone.2427c94d.png"
+  },
+  {
+    "revision": "f5cbbe1dc42d376c87e20477cb3c9303",
+    "url": "/MultisiteON/static/media/idea.f5cbbe1d.png"
+  },
+  {
+    "revision": "53b834909450a16de8448adb872c4684",
+    "url": "/MultisiteON/static/media/nube.53b83490.png"
+  }
+]);
